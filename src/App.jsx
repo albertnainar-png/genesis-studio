06f4e9fb9a9863b0a8aa7c1805b6cc628ba4495d
Albert Nainar <albertnainar@gmail.com>
2026-09-15 03:33:25 +0530

    Use HashRouter for GitHub Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Dashboard from "./pages/Dashboard";
 import Studio from "./pages/Studio";
 
 function App() {
-  return <BrowserRouter><Routes><Route path="/" element={<Landing />} /><Route path="/dashboard" element={<Dashboard />} /><Route path="/studio" element={<Studio />} /></Routes></BrowserRouter>;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/studio" element={<Studio />} />
+      </Routes>
+    </HashRouter>
+  );
 }
+
 export default App;
