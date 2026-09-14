e564d7f712076927db68cfc78a6113b7fa5c18ae
Albert Nainar <albertnainar@gmail.com>
2026-09-15 03:24:15 +0530

    feat: connect Genesis Studio workspace route

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Landing from "./pages/Landing";
 import Dashboard from "./pages/Dashboard";
+import Studio from "./pages/Studio";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <BrowserRouter><Routes><Route path="/" element={<Landing />} /><Route path="/dashboard" element={<Dashboard />} /><Route path="/studio" element={<Studio />} /></Routes></BrowserRouter>;
 }
-
 export default App;
